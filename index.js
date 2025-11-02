a0ff481f8e7004fe90ededdd3aae3db9156c7e8c
gooblet <awesomerocket1000@gmail.com>
2025-11-01 22:26:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
   window.addEventListener('DOMContentLoaded', (event) => {
-    const encodedPasswords = ["MjM0MTQyMzQ=", "R01TMjAyNQ==", "bGltYnVzY29tcGFueQ==", "bWFpbnRlbmFuY2VmcmlkYXk="];
-    const encodedPasswords2 = ["cGll", "Y3Vt"];
-    const HASH_PASS1 = "c15228d5f64658de951afa8dfe6c227fb4c9bbe8caf9f35b10153ddb493ab516";
+    const HASH_PASS1 = "c08294bf5c5751df6e988281e051b9c5363c55dfaa037559a7b2e62996751221";
     const HASH_PASS2 = "558211ed72b2d6967037419dff6f1e7cfd002d178c8fdeeb1239760d4e4c4059";
 
     const storedPassword = sessionStorage.getItem('auth1');
